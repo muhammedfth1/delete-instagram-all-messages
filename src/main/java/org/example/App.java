5c298d1cbf98ab7c095232fa5ff69f3e5288fe22
muhammedfth1 <92013695+muhammedfth1@users.noreply.github.com>
2022-09-03 18:23:56 +0300

    locators found , created wait and click function.

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class App {
+    WebDriver driver;
+    String base_url = "https://www.instagram.com/";
+    By usernameLocator = new By.ByCssSelector("input[name='username']");
+    By passwordLocator = new By.ByCssSelector("input[name='password']");
+    By loginButtonLocator = new By.ByCssSelector("button[type='submit']");
+    By goMessages = By.className("xWeGp");
+    By nextControl = By.className("_a9_1");
+    By messagesBox = By.className("_aad7");
+    By attentionLocator = new By.ByCssSelector("circle[cx='11.819']");
+    By firstDeleteButton = By.xpath("//*[text()='Delete Chat']");
+    By secondDeleteButton = By.xpath("//*[text()='Delete']");
+    By waitNextMessage = By.xpath("//*[text()='Your Messages']");
+
+    public App(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get(base_url);
+        driver.manage().window().maximize();
+    }
+
+    public void locatorWait(By Locator){
+        WebDriverWait wait = new WebDriverWait(driver , Duration.ofSeconds(500));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Locator));
+    }
+
+    public void forClick(By Locator){
+        driver.findElement(Locator).click();
+    }
+}
