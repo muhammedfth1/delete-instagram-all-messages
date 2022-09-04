af191bb0a9f6f4d54615530f70f53fe573dfef2f
muhammedfth1 <92013695+muhammedfth1@users.noreply.github.com>
2022-09-04 13:19:30 +0300

    created main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Kullanıcı adiniz : ");
+        String username = scanner.nextLine();
+        System.out.println("Sifreniz : ");
+        String password = scanner.nextLine();
+        App app = new App();
+        app.login(username , password);
+        app.goToMessagesBox();
+        app.deleteMessages();
     }
 }
